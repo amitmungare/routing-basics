3d7d0de137ddf0274b40d3f880528d758f72c097
amitmungare <amungare27@gmail.com>
2024-07-28 14:05:13 +0530

    complete routing basics

--- src/pages/Products.js
+++ src/pages/Products.js
@@ -1,17 +1,25 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
+const PRODUCTS = [
+  {id:'p1', title:'p 1'},
+  {id:'p2', title:'p 2'},
+  {id:'p3', title:'p 3'},
+  {id:'p4', title:'p 4'},
+]
+
 const Products = () => {
   return (
     <>
       <h1>Products</h1>
       <ul>
-        <li>p1</li>
-        <li>p2</li>
-        <li>p3</li>
-        <li>p4</li>
-        <li>p5</li>
-        <li>p6</li>
+          {PRODUCTS.map((prod)=>(
+            <li key={prod.id}>
+              <Link to={`/products/${prod.id}`}>
+                {prod.title}
+              </Link>
+            </li>
+          ))}
       </ul>
     </>
   );
